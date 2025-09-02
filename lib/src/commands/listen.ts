b0c645aa306a0395902cc9cb4f7ba620eb0d7c28
YuzuharaYuka <128817934+YuzuharaYuka@users.noreply.github.com>
2025-09-02 21:15:59 +0800

    Delete lib/src directory

--- lib/src/commands/listen.ts
+++
@@ -1,9 +0,0 @@
-// --- START OF FILE src/commands/listen.ts ---
-
-import { Context } from 'koishi'
-import { CommandHandler } from './handler'
-
-export function registerListenCommand(ctx: Context, handler: CommandHandler) {
-  ctx.command('听音声 <query:text>', '获取并收听音声')
-    .action(async ({ session }, query) => handler.handleListen(session, query));
-}
--- lib/src/commands/popular.ts
+++
@@ -1,9 +0,0 @@
-// --- START OF FILE src/commands/popular.ts ---
-
-import { Context } from 'koishi'
-import { CommandHandler } from './handler'
-
-export function registerPopularCommand(ctx: Context, handler: CommandHandler) {
-  ctx.command('热门音声 [page:number]', '获取当前热门音声列表')
-    .action(async ({ session }, page) => handler.handlePopular(session, page));
-}
